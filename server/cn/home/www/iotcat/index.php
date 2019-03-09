746dc27488cba609b534a41f6a8b643e3e0dce07
IoTgod <i@IoTgod.me>
2019-03-09 20:06:29 +0800

    trans ssl

--- server/cn/home/www/iotcat/index.php
+++ server/cn/home/www/iotcat/index.php
@@ -8,6 +8,6 @@ echo "<script src=\"https://cdn.yimian.ac.cn/hhCandy/copy.js\"></script>";
 
 yimian__headerEnd();
 
-echo file_get_contents("./etc/hhCandy/index.html");
+echo file_get_contents("../etc/hhCandy/index.html");
 
 yimian__simpleFooter(1);
--- server/cn/home/www/tomato/usr/plugins/WikimoeBangumi/README.md
+++
@@ -1,5 +0,0 @@
-# Typecho追番插件
-
-#### 项目介绍
-本插件为通过Bangumi的API来获取追番信息。适用于Typecho!  
-具体使用方法相见：[https://www.wikimoe.com/?post=136](https://www.wikimoe.com/?post=136)
