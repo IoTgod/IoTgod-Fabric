625b24622977811906f3bc3e0190ce5d495f62c2
IoTgod <i@IoTgod.me>
2019-03-09 23:38:18 +0800

    fp cookie

--- server/cn/home/www/functions.php
+++ server/cn/home/www/functions.php
@@ -118,7 +118,7 @@ function js__cleverLogin()
 function yimian__heartbeat()
 {
 	echo "
-<script>$.post(\"https://log.yimian.xyz/heartbeat.php\",{\"ip\":ip,\"fp\":fp,\"city\":ipCity})</script>";
+<script>$.post(\"https://log.yimian.xyz/heartbeat.php\",{\"ip\":ip,\"fp\":fp,\"city\":ipCity});cookie.set('fp',fp);</script>";
 }
 
 
--- server/cn/home/www/video/index.php
+++ server/cn/home/www/video/index.php
@@ -3,7 +3,7 @@ include '../functions.php';
 
 video__bodyUp();
 
-echo file_get_contents("./mainlist.html");
+echo file_get_contents("https://video.yimian.xyz/mainlist.html");
 
 video__bodyDown();
 
