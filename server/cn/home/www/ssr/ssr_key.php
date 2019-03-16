3055edbbade54651875349c5c127999c80cf8c83
IoTcat <i@iotcat.me>
2019-03-16 22:34:17 +0800

    add pixiv ssr

--- server/cn/home/www/ssr/ssr_key.php
+++ server/cn/home/www/ssr/ssr_key.php
@@ -16,11 +16,13 @@ $passwd=$row[0]['passwd'];
 
 $str=base64_encode("aes-256-cfb:$passwd@usa.ssr.yimian.xyz:$port")."
 ss://".base64_encode("aes-256-cfb:$passwd@aus.ssr.yimian.xyz:$port")."
-ss://".base64_encode("aes-256-cfb:$passwd@ssr.yimian.xyz:$port");
+ss://".base64_encode("aes-256-cfb:$passwd@ssr.yimian.xyz:$port").
+"ss://".base64_encode("aes-256-cfb:$passwd@pixiv.ssr.yimian.xyz:$port");;
 
 $str1=base64_encode("aes-256-cfb:$passwd@usa.ssr.yimian.xyz:$port");
 $str2=base64_encode("aes-256-cfb:$passwd@aus.ssr.yimian.xyz:$port");
 $str3=base64_encode("aes-256-cfb:$passwd@ssr.yimian.xyz:$port");
+$str4=base64_encode("aes-256-cfb:$passwd@pixiv.ssr.yimian.xyz:$port");
 
 
 yimian__header("Yimain VPN");
@@ -37,6 +39,7 @@ yimian__headerEnd();
 <p>你的首选密钥是：ss://<?php echo $str3?></p>
 <p>美国服务器密钥：ss://<?php echo $str1?></p>
 <p>澳大利亚服务器密钥：ss://<?php echo $str2?></p>
+<p>P站优化专线：ss://<?php echo $str4?></p>
 
 <div><span id="btn" data-clipboard-text="ss://<?php echo $str?>"><button>点击复制全部</button></span></div>
 <div id="show" style="display: none;">已复制</div>
