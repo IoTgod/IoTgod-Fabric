253c176b09161bf8d747eed6a3eb37e1edd650be
IoTcat <i@iotcat.me>
2019-03-22 18:31:24 +0800

    fix mail

--- server/cn/home/www/mail/index.php
+++ server/cn/home/www/mail/index.php
@@ -3,7 +3,7 @@
 include '../functions.php';
 
 
-echo json_encode(array("state" => yimian__mail($_REQUEST['to'], $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['fromName'])));
+echo json_encode(array("state" => yimian__mail($_REQUEST['to'], $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['from'])));
 
 yimian__log("log_api", array("api" => "mail", "timestamp" => date('Y-m-d H:i:s', time()), "ip" => ip2long(get_ip()), "_from" => get_from(), "content" => $_REQUEST['subject']));
 
