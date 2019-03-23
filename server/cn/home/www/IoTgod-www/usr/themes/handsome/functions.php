6ab76b11b1ef8dbb1bbdef8bf2502a7df2c2c471
IoTcat <i@iotcat.me>
2019-03-23 22:18:07 +0800

    handsome 随机小图

--- server/cn/home/www/IoTgod-www/usr/themes/handsome/functions.php
+++ server/cn/home/www/IoTgod-www/usr/themes/handsome/functions.php
@@ -697,8 +697,8 @@ WebP
 function showSidebarThumbnail($widget, $index = 0)
 {
 
-    $randomNum = unserialize(SIDEBAR_IMAGE_ARRAY);
-    $random = THEME_URL . 'usr/img/sj2/' . $randomNum[$index] . '.jpg'; // 随机缩略图路径
+    //$randomNum = unserialize(SIDEBAR_IMAGE_ARRAY);
+    $random = /*THEME_URL . */'https://image.yimian.xyz/acg/head/'. /*$randomNum[$index] */mt_rand(1,130). '.png'; // 随机缩略图路径
     //正则匹配 主题目录下的/images/sj2/的图片（以数字按顺序命名）
 
     return $random;
