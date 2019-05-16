52cd7528351a0ec0f66bef3b4c5a3f4df04fff41
IoTcat <i@iotcat.me>
2019-05-16 14:44:35 +0800

    change head

--- server/cn/home/www/wiot/ota/check.php
+++ server/cn/home/www/wiot/ota/check.php
@@ -4,6 +4,7 @@ include '../../functions.php';
 header('Access-Control-Allow-Origin:*');
 
 $version = $_REQUEST['version'];
+$mac = $_REQUEST['MAC'];
 
 if(isset($version)){
     if($version != "v0.1.4")
@@ -11,7 +12,7 @@ if(isset($version)){
     else
         echo json_encode(array('update' => 0));
 
-yimian__log("log_wiot", array("timestamp" => date('Y-m-d H:i:s', time()), "ip" => ip2long(get_ip()), "version" => $version /* 经md5不可逆加密，仅用于区分用户 */));
+yimian__log("log_wiot", array("timestamp" => date('Y-m-d H:i:s', time()), "ip" => ip2long(get_ip()), "version" => $version /* 经md5不可逆加密，仅用于区分用户 */, "mac" => $mac));
 
     die();
 }
